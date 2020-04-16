5e9da5f4c6267dbae89e7f7de12df3bdf55f7f61
michaeltconnelly <mconnelly@rsmas.miami.edu>
2020-04-16 07:40:55 -0400

    first test salmon

--- bash/salmon_quantify_symC1.sh
+++ bash/salmon_quantify_symC1.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 #purpose: quantify non-coral trimmed RNAseq reads against the Cladocopium C1 reference transcriptome (Levin et al. 2016)
+#conda activate salmon
+salmon index -t ./data/refs/GSE72763_MI_min250_nr.fasta.gz -i ./data/indices/symC1_MI_index
+
+salmon quant -i ./data/indices/symC1_MI_index \
+-l SR \
+-r ./data/reads/Wt2-6b_PdamUnmapped.out.mate1.fastq \
+--validateMappings -o ./outputs/transcripts_quant
