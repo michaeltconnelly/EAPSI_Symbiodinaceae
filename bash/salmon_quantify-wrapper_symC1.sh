31aee1accf9ac9078b44dcb759eb30914324ba31
michaeltconnelly <mconnelly@rsmas.miami.edu>
2020-04-17 09:34:37 -0400

    fixed salmon script

--- bash/salmon_quantify-wrapper_symC1.sh
+++ bash/salmon_quantify-wrapper_symC1.sh
@@ -15,7 +15,6 @@
 #specify variable containing sequence file prefixes and directory paths
 mcs="/scratch/projects/transcriptomics/mikeconnelly"
 prodir="/scratch/projects/transcriptomics/mikeconnelly/projects/EAPSI_Symbiodinaceae"
-exp="AxH"
 EAPSIsamples="Wt1-1a Wt1-1b Wt1-1c Wt1-4a Wt1-4b Wt1-4c Wt1-5a Wt1-5b Wt1-5c Wt1-6a Wt1-6b Wt1-6c Wt2-1a Wt2-1b Wt2-1c Wt2-4a Wt2-4b Wt2-4c Wt2-5a Wt2-5b Wt2-5c Wt2-6a Wt2-6b Wt2-6c Hw1-1a Hw1-1b Hw1-1c Hw1-4a Hw1-4b Hw1-4c Hw1-5a Hw1-5b Hw1-5c Hw1-6a Hw1-6b Hw1-6c Hw2-1a Hw2-1b Hw2-1c Hw2-4a Hw2-4b Hw2-4c Hw2-5a Hw2-5b Hw2-5c Hw2-6b Hw2-6c"
 
 #lets me know which files are being processed
@@ -30,13 +29,13 @@ echo "Quantifying ${EAPSIsample}"
 echo '#!/bin/bash' > "${prodir}"/bash/jobs/"${EAPSIsample}"_salmonquant_symC1.job
 echo '#BSUB -q general' >> "${prodir}"/bash/jobs/"${EAPSIsample}"_salmonquant_symC1.job
 echo '#BSUB -J '"${EAPSIsample}"_salmonquant_symC1'' >> "${prodir}"/bash/jobs/"${EAPSIsample}"_salmonquant_symC1.job
-echo '#BSUB -o '"${coldir}"/"${exp}"/logfiles/"$EAPSIsample"_salmonquant_symC1%J.out'' >> "${prodir}"/bash/jobs/"${EAPSIsample}"_salmonquant_symC1.job
-echo '#BSUB -e '"${coldir}"/"${exp}"/errorfiles/"$EAPSIsample"_salmonquant_symC1%J.err'' >> "${prodir}"/bash/jobs/"${EAPSIsample}"_salmonquant_symC1.job
+echo '#BSUB -o '"${prodir}"/logfiles/"$EAPSIsample"_salmonquant_symC1%J.out'' >> "${prodir}"/bash/jobs/"${EAPSIsample}"_salmonquant_symC1.job
+echo '#BSUB -e '"${prodir}"/errorfiles/"$EAPSIsample"_salmonquant_symC1%J.err'' >> "${prodir}"/bash/jobs/"${EAPSIsample}"_salmonquant_symC1.job
 echo '#BSUB -n 8' >> "${prodir}"/bash/jobs/"${EAPSIsample}"_salmonquant_symC1.job
 echo '#BSUB -W 4:00' >> "${prodir}"/bash/jobs/"${EAPSIsample}"_salmonquant_symC1.job
 
 #   input command to run salmon
-echo salmon quant \
+echo "${prodir}"/programs/salmon-latest_linux_x86_64/bin/salmon quant \
 -i ${prodir}/data/indices/symC1_MI_index \
 -l SR \
 -r ${prodir}/outputs/STARalign_Pdam/${EAPSIsample}_PdamUnmapped.out.mate1.fastq \
